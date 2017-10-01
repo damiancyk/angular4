712ae5815abb45c68f97d2351d0ab5ced742a615
Damian Pieta <damiancyk@gmail.com>
2017-10-01 16:42:57 +0200

    First component + service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PeopleService } from './people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PeopleService]
 })
 export class AppComponent {
   title = 'app';

34127af592bf410d207ccd048c33110e51538a6a
Damian Pieta <damiancyk@gmail.com>
2017-10-04 00:21:38 +0200

    Routing just added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { PeopleService } from './people.service';
 import { PersonDetailsComponent } from './person-details/person-details.component';
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -12,9 +17,12 @@ import { PersonDetailsComponent } from './person-details/person-details.componen
     PersonDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit, Input  } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+import { PeopleService } from "../people.service";
 import { Person } from "../person";
 
 @Component({
   selector: 'app-person-details',
   template: `
-
-  <!-- we moved the details template here -->
   <section *ngIf="person">
-    <h2>You selected: {{person.name}}</h2>
+    <h2>You selected:  {{person.name}}</h2>
     <h3>Description</h3>
     <p>
        {{person.name}} weights {{person.weight}} and is {{person.height}} tall.
     </p>
   </section>
-
+  <! -- NEW BUTTON HERE! -->
+  <button (click)="gotoPeoplesList()">Back to peoples list</button>
   `,
   styles: []
 })
-export class PersonDetailsComponent implements OnInit {
-  @Input() person: Person;
+export class PersonDetailsComponent implements OnInit, OnDestroy {
+  person: Person;
+  sub:any;
 
-  constructor() { }
+  constructor(private peopleService: PeopleService,
+                  private route: ActivatedRoute,
+                  private router: Router){}
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = Number.parseInt(params['id']);
+      this.person = this.peopleService.get(id);
+    });
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  gotoPeoplesList(){
+    let link = ['/persons'];
+    this.router.navigate(link);
+}
 
 }

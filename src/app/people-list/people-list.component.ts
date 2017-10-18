3a8eedfdb46b51b6b9ea9534a8d078eec4c4dff1
Damian Pieta <damiancyk@gmail.com>
2017-10-18 11:11:09 +0200

    error handling

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,26 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from '../person';
-import { PeopleService } from '../people.service';
+import { PeopleService } from "../people.service";
 
 @Component({
   selector: 'app-people-list',
-  templateUrl: './people-list.component.html',
+  template: `
+
+  <section *ngIf="isLoading && !errorMessage">
+  Loading our hyperdrives!!! Retrieving data...
+  </section>
+  <!-- this is the new syntax for ng-repeat -->
+  <ul>
+    <li *ngFor="let person of people">
+      <a [routerLink]="['/persons', person.id]">
+        {{person.name}}
+      </a>
+    </li>
+  </ul>
+  <!-- HERE: added this error message -->
+  <section *ngIf="errorMessage">
+    {{errorMessage}}
+  </section>
+  `,
   styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent implements OnInit {
-people: Person[] = [];
-selectedPerson: Person;
+  people: Person[] = [];
+  errorMessage: string = '';
+  isLoading: boolean = true;
 
-constructor(private peopleService: PeopleService) { }
+  constructor(private peopleService: PeopleService) { }
 
-  ngOnInit() {
-     this.peopleService
-         .getAll()
-         .subscribe(p => this.people = p);
-   }
-
-  selectPerson(person: Person){
-      this.selectedPerson = person;
+  ngOnInit(){
+    this.peopleService
+      .getAll()
+      .subscribe(
+         /* happy path */ p => this.people = p,
+         /* error path */ e => this.errorMessage = e,
+         /* onCompleted */ () => this.isLoading = false);
   }
 
 }
--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -3,10 +3,10 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Person } from './person';
-
-const baseUrl: string = 'https://swapi.co/api';
 
 const PEOPLE : Person[] = [
       {id: 1, name: 'Luke Skywalker', height: 177, weight: 70},
@@ -14,72 +14,87 @@ const PEOPLE : Person[] = [
       {id: 3, name: 'Han Solo', height: 185, weight: 85},
     ];
 
-    @Injectable()
-    export class PeopleService{
+@Injectable()
+export class PeopleService{
+  private baseUrl: string = 'https://swapi.co/api';
+  constructor(private http : Http){
+  }
+
+  getAll(): Observable<Person[]>{
+    let people$ = this.http
+      .get(`${this.baseUrl}/people`, { headers: this.getHeaders()})
+      .map(mapPersons)
+      .catch(handleError);
+      return people$;
+  }
+
+  private getHeaders(){
+    // I included these headers because otherwise FireFox
+    // will request text/html
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    return headers;
+  }
+  get(id: number): Observable<Person> {
+    let person$ = this.http
+      .get(`${this.baseUrl}/people/${id}`, {headers: this.getHeaders()})
+      .map(mapPerson)
+      .catch(handleError);
+      return person$;
+  }
+
+  save(person: Person) : Observable<Response>{
+    // this won't actually work because the StarWars API doesn't
+    // is read-only. But it would look like this:
+    return this
+      .http
+      .put(`${this.baseUrl}/people/${person.id}`,
+            JSON.stringify(person),
+            {headers: this.getHeaders()});
+  }
+
+}
 
 
-      constructor(private http : Http){}
+function mapPersons(response:Response): Person[]{
+  //throw new Error('ups! Force choke!');
 
-      getAll(): Observable<Person[]>{
-        let people$ = this.http
-          .get(`${baseUrl}/people`, { headers: this.getHeaders()})
-          .map(mapPersons);
-          return people$;
-      }
+  // The response of the API has a results
+  // property with the actual results
+  return response.json().results.map(toPerson)
+}
 
-      private getHeaders(){
-        // I included these headers because otherwise FireFox
-        // will request text/html
-        let headers = new Headers();
-        headers.append('Accept', 'application/json');
-        return headers;
-      }
-      get(id: number): Observable<Person> {
-        let person$ = this.http
-          .get(`${baseUrl}/people/${id}`, {headers: this.getHeaders()})
-          .map(mapPerson);
-          return person$;
-      }
+function toPerson(r:any): Person{
+  let person = <Person>({
+    id: extractId(r),
+    url: r.url,
+    name: r.name,
+    weight: Number.parseInt(r.mass),
+    height: Number.parseInt(r.height),
+  });
+  console.log('Parsed person:', person);
+  return person;
+}
 
-      save(person: Person) : Observable<Response>{
-        // this won't actually work because the StarWars API
-        // is read-only. But it would look like this:
-        return this
-          .http
-          .put(`${baseUrl}/people/${person.id}`,
-                JSON.stringify(person),
-                {headers: this.getHeaders()});
-      }
+// to avoid breaking the rest of our app
+// I extract the id from the person url
+function extractId(personData:any){
+  let extractedId = personData.url.replace('https://swapi.co/api/people/','').replace('/','');
+  return parseInt(extractedId);
+}
 
-    }
+function mapPerson(response:Response): Person{
+   // toPerson looks just like in the previous example
+   return toPerson(response.json());
+}
 
+// this could also be a private method of the component class
+function handleError (error: any) {
+  // log error
+  // could be something more sofisticated
+  let errorMsg = error.message || `Yikes! There was a problem with our hyperdrive device and we couldn't retrieve your data!`
+  console.error(errorMsg);
 
-    function mapPersons(response:Response): Person[]{
-      // The response of the API has a results
-      // property with the actual results
-      return response.json().results.map(toPerson)
-    }
-
-    function toPerson(r:any): Person{
-      let person = <Person>({
-        id: extractId(r),
-        url: r.url,
-        name: r.name,
-        weight: Number.parseInt(r.mass),
-        height: Number.parseInt(r.height),
-      });
-      console.log('Parsed person:', person);
-      return person;
-    }
-
-    // to avoid breaking the rest of our app
-    // I extract the id from the person url
-    function extractId(personData:any){
-      let extractedId = personData.url.replace(baseUrl+'/people/','').replace('/','');
-      return parseInt(extractedId);
-    }
-
-    function mapPerson(response:Response): Person{
-       // toPerson looks just like in the previous example
-       return toPerson(response.json());
-    }
+  // throw an application level error
+  return Observable.throw(errorMsg);
+}
